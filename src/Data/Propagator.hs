b8622e8440e3b53c627f6d34b0ae192b1a72ffff
Jesse Holzman <jesseholzman96@gmail.com>
2024-07-22 21:50:36 -0400

    Add more cell functions and rename some

--- src/Data/Propagator.hs
+++ src/Data/Propagator.hs
@@ -7,25 +7,34 @@ import Data.Propagator.Class
 data Cell s a = Cell
   { _update :: a -> a -> UpdateResult a,
     _val :: MutVar s a,
-    _propOut :: MutVar s (a -> ST s ())
+    _subs :: MutVar s (a -> ST s ())
   }
 
-emptyCell :: (PartialInfo a) => ST s (Cell s a)
-emptyCell = emptyCellWith update
+empty :: (PartialInfo a) => ST s (Cell s a)
+empty = emptyWith update
 
-emptyCellWith :: (PartialInfo a) => (a -> a -> UpdateResult a) -> ST s (Cell s a)
-emptyCellWith mrg = Cell mrg <$> newMutVar leastInfo <*> newMutVar (\_ -> pure ())
+emptyWith :: (PartialInfo a) => (a -> a -> UpdateResult a) -> ST s (Cell s a)
+emptyWith = filledWith leastInfo
 
-readCell :: Cell s a -> ST s a
-readCell cell = readMutVar (_val cell)
+filled :: (PartialInfo a) => a -> ST s (Cell s a)
+filled = flip filledWith update
 
-writeCell :: Cell s a -> a -> ST s ()
-writeCell cell new = do
+filledWith :: (PartialInfo a) => a -> (a -> a -> UpdateResult a) -> ST s (Cell s a)
+filledWith val upd = Cell upd <$> newMutVar val <*> newMutVar (\_ -> pure ())
+
+peek :: Cell s a -> ST s a
+peek cell = readMutVar (_val cell)
+
+push :: Cell s a -> a -> ST s ()
+push cell new = do
   old <- readMutVar (_val cell)
   case _update cell old new of
     Unchanged _ -> pure ()
     Changed a -> do
       writeMutVar (_val cell) a
-      prop <- readMutVar (_propOut cell)
+      prop <- readMutVar (_subs cell)
       prop a
     Contradiction -> error "uh oh!"
+
+watch :: Cell s a -> (a -> ST s ()) -> ST s ()
+watch cell newsub = modifyMutVar' (_subs cell) (\subs a -> subs a >> newsub a)
