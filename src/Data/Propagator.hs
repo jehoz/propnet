2979123f856a0220ebce977d9d38c62d73b1077d
Jesse Holzman <jesseholzman96@gmail.com>
2024-07-22 23:15:56 -0400

    Add docstrings

--- src/Data/Propagator.hs
+++ src/Data/Propagator.hs
@@ -4,27 +4,38 @@ import Control.Monad.ST (ST)
 import Data.Primitive.MutVar
 import Data.Propagator.Class
 
+-- | Cells hold partial information about a value and propagates
 data Cell s a = Cell
   { _update :: a -> a -> UpdateResult a,
     _val :: MutVar s a,
     _subs :: MutVar s (a -> ST s ())
   }
 
+-- | Creates a new cell with the default update function and least information
+-- value for the type.
 empty :: (PartialInfo a) => ST s (Cell s a)
 empty = emptyWith update
 
+-- | Creates a new cell with least information value and a custom update
+-- function.
 emptyWith :: (PartialInfo a) => (a -> a -> UpdateResult a) -> ST s (Cell s a)
 emptyWith = filledWith leastInfo
 
+-- | Creates a new cell with the default update function, initialized with a
+-- custom initial value.
 filled :: (PartialInfo a) => a -> ST s (Cell s a)
 filled = flip filledWith update
 
+-- | Creates a new cell with both custom initial value and update function.
 filledWith :: (PartialInfo a) => a -> (a -> a -> UpdateResult a) -> ST s (Cell s a)
 filledWith val upd = Cell upd <$> newMutVar val <*> newMutVar (\_ -> pure ())
 
+-- | Get the current value of a cell
 peek :: Cell s a -> ST s a
 peek cell = readMutVar (_val cell)
 
+-- | Push a new value into a cell.  The cell's current value is updated and
+-- propagated to all subscribers.
 push :: Cell s a -> a -> ST s ()
 push cell new = do
   old <- readMutVar (_val cell)
@@ -36,5 +47,7 @@ push cell new = do
       prop a
     Contradiction -> error "uh oh!"
 
+-- | Register a subscription function for a cell.  When the cell's value is
+-- changed, subscription functions are called with the new value.
 watch :: Cell s a -> (a -> ST s ()) -> ST s ()
 watch cell newsub = modifyMutVar' (_subs cell) (\subs a -> subs a >> newsub a)
--- src/Data/Propagator/Class.hs
+++ src/Data/Propagator/Class.hs
@@ -1,12 +1,24 @@
 module Data.Propagator.Class where
 
+-- | Representation of the different outcomes after updating a cell's value.
 data UpdateResult a = Unchanged a | Changed a | Contradiction
 
+-- | Class for a type that carries partial information about a type.
+--
+-- Should behave as a bounded join semilattice with a bottom value representing
+-- the least amount of information about a value.  Updating the value with more
+-- information moves up the lattice, ideally culminating in a fully defined
+-- value.  Updating with conflicting information should produce a contradiction.
 class PartialInfo a where
+  -- | The value of "least information" for the type, ie. the bottom of the lattice.
   leastInfo :: a
 
+  -- | Merge the current value (first argument) with a new incoming value
+  -- (second argument).
   update :: a -> a -> UpdateResult a
 
+-- | Maybe is the simplest partial information type, containing  either no
+-- information or complete information about a value.
 instance (Eq a) => PartialInfo (Maybe a) where
   leastInfo = Nothing
 
