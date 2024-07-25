0617427670eaf4ee93c7b93461d1f4fc3e6b40e3
Jesse Holzman <jesseholzman96@gmail.com>
2024-07-24 22:53:46 -0400

    Use overloaded record dot syntax

--- src/Data/Propagator/Cell.hs
+++ src/Data/Propagator/Cell.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE NoFieldSelectors #-}
+
 module Data.Propagator.Cell where
 
 import Control.Monad.ST (ST)
@@ -6,9 +9,9 @@ import Data.Propagator.Class
 
 -- | Cells hold partial information about a value and propagates
 data Cell s a = Cell
-  { _update :: a -> a -> UpdateResult a,
-    _val :: MutVar s a,
-    _subs :: MutVar s (a -> ST s ())
+  { update :: a -> a -> UpdateResult a,
+    value :: MutVar s a,
+    subs :: MutVar s (a -> ST s ())
   }
 
 -- | Creates a new cell with the default update function and least information
@@ -19,7 +22,7 @@ empty = emptyWith update
 -- | Creates a new cell with least information value and a custom update
 -- function.
 emptyWith :: (PartialInfo a) => (a -> a -> UpdateResult a) -> ST s (Cell s a)
-emptyWith = filledWith leastInfo
+emptyWith = filledWith bottom
 
 -- | Creates a new cell with the default update function, initialized with a
 -- custom initial value.
@@ -32,25 +35,25 @@ filledWith val upd = Cell upd <$> newMutVar val <*> newMutVar (\_ -> pure ())
 
 -- | Get the current value of a cell
 peek :: Cell s a -> ST s a
-peek cell = readMutVar (_val cell)
+peek cell = readMutVar cell.value
 
 -- | Push a new value into a cell.  The cell's current value is updated and
 -- propagated to all subscribers.
 push :: Cell s a -> a -> ST s ()
 push cell new = do
-  old <- readMutVar (_val cell)
-  case _update cell old new of
+  old <- readMutVar cell.value
+  case cell.update old new of
     Unchanged -> pure ()
     Changed a -> do
-      writeMutVar (_val cell) a
-      prop <- readMutVar (_subs cell)
+      writeMutVar cell.value a
+      prop <- readMutVar cell.subs
       prop a
     Contradiction -> error "uh oh!"
 
 -- | Register a subscription function for a cell.  When the cell's value is
 -- changed, subscription functions are called with the new value.
 watch :: Cell s a -> (a -> ST s ()) -> ST s ()
-watch cell newsub = modifyMutVar' (_subs cell) (\notify a -> notify a >> newsub a)
+watch cell newsub = modifyMutVar' cell.subs (\notify a -> notify a >> newsub a)
 
 -- | Perform an action with the value of a cell.  Like `watch` but instead of
 -- running whenever the cell changes, runs once immediately.
