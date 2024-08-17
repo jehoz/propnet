0037e72d9af7c027e8c2c388b3b19a2dd8c1df87
holzmanj <jesseholzman96@gmail.com>
2024-08-17 19:29:33 -0400

    Select example with command line arg (only sudoku right now)

--- examples/Main.hs
+++ examples/Main.hs
@@ -1,6 +1,16 @@
 module Main (main) where
 
 import qualified Sudoku
+import System.Environment
+import Text.Printf (printf)
+
+runExample :: String -> IO ()
+runExample "sudoku" = Sudoku.main
+runExample other = printf "No example named \"%s\"" other
 
 main :: IO ()
-main = Sudoku.main
+main = do
+  args <- getArgs
+  case args of
+    [] -> putStrLn "Provide the name of the example you want to run."
+    a : _ -> runExample a
