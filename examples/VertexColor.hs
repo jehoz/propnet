d214084fba50b4dcc20b2f5765976913b6e2ea43
holzmanj <jesseholzman96@gmail.com>
2024-08-17 22:39:35 -0400

    Generate petersen graph instead of k5

--- examples/VertexColor.hs
+++ examples/VertexColor.hs
@@ -6,18 +6,23 @@ import Diagrams.Prelude hiding (Color)
 data Color = Blue | Green | Red deriving (Eq, Bounded, Enum)
 
 vert :: Color -> Int -> Diagram B
-vert c n = circle 0.2 # fc (toFc c) # named n
+vert c n = circle 0.1 # fc (toFc c) # named n
   where
     toFc Blue = blue
     toFc Green = green
     toFc Red = red
 
-coloredGraph :: [Color] -> Diagram B
-coloredGraph cs =
-  let n = length cs
-      aopts = with & arrowHead .~ noHead & arrowTail .~ noTail
-   in atPoints (trailVertices $ regPoly n 1) (zipWith vert cs [1 ..])
-        # applyAll [connectOutside' aopts i j | i <- [1 .. n - 1], j <- [i + 1 .. n]]
+edges :: [a] -> [(a, a)]
+edges xs =
+  let outer = take 5 xs
+      inner = drop 5 xs
+   in zip outer (drop 1 (cycle outer)) ++ zip inner (drop 2 (cycle inner)) ++ zip outer inner
+
+petersenGraph :: [Color] -> Diagram B
+petersenGraph cs =
+  let aopts = with & arrowHead .~ noHead & arrowTail .~ noTail
+   in atPoints (trailVertices (regPoly 5 1) ++ trailVertices (regPoly 5 0.5)) (zipWith vert cs [1 ..])
+        # applyAll [connectOutside' aopts i j | (i, j) <- edges [1 .. (10 :: Int)]]
 
 main :: IO ()
-main = renderSVG "vertex-color.svg" (dims2D 400 400) (coloredGraph [Blue, Green, Red, Blue, Green])
+main = renderSVG "vertex-color.svg" (dims2D 400 400) (petersenGraph (replicate 10 Blue))
