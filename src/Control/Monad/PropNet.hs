2fff1eddcdbf8b648b2680f498a8d813e4284775
holzmanj <jesseholzman96@gmail.com>
2024-08-24 10:10:50 -0400

    Add random shuffling functions, and alternative search that doesn't use TMS

--- src/Control/Monad/PropNet.hs
+++ src/Control/Monad/PropNet.hs
@@ -6,7 +6,8 @@
 
 module Control.Monad.PropNet where
 
-import Control.Monad ((>=>))
+import Control.Applicative (Alternative (empty), asum)
+import Control.Monad (forM, liftM2, replicateM, zipWithM, zipWithM_, (>=>))
 import Control.Monad.IO.Class (MonadIO, liftIO)
 import Control.Monad.Primitive (PrimMonad (primitive), PrimState)
 import Control.Monad.PropNet.Class (LogicCell, MonadPropNet (..))
@@ -20,14 +21,15 @@ import qualified Data.HashSet as HashSet
 import Data.Kind (Type)
 import Data.List (minimumBy)
 import Data.Maybe (catMaybes, fromJust)
-import Data.Primitive (MutVar, newMutVar, readMutVar, writeMutVar)
+import Data.Primitive (MutVar, modifyMutVar, newMutVar, readMutVar, writeMutVar)
 import Data.PropNet.Partial (Partial (..), UpdateResult (..), update)
 import Data.PropNet.Partial.OneOf (OneOf, only)
 import qualified Data.PropNet.Partial.OneOf as OneOf
-import Data.PropNet.TMS (Assumption (..), Premise, TMS (..), consequentOf, deepestBranch)
+import Data.PropNet.TMS (Assumption (..), Premise, TMS (..), addAssumption, consequentOf, deepestBranch)
 import qualified Data.PropNet.TMS as TMS
 import Data.Traversable (for)
 import System.Random (Random (randomR), StdGen, initStdGen, mkStdGen)
+import qualified System.Random.Shuffle as Shuffle
 
 data PropNetState = PropNetState
   { -- | Incrementing counter for assigning each new cell a unique 'Name'
@@ -88,6 +90,10 @@ evalPropNetT p = evalStateT p.unPropNetT initialState
 initialState :: PropNetState
 initialState = PropNetState {nameCounter = 0, rng = mkStdGen 1123, contradiction = False}
 
+-- | Replace the value of a cell without triggering it's propagators
+replace :: (PrimMonad m) => Cell (PropNetT m) a -> a -> PropNetT m ()
+replace cell v = modifyMutVar cell.body (\(_, ns) -> (v, ns))
+
 -- | Emits a new (unique) cell ID.  This should be called once for each new
 -- cell that gets created so that each has a unique ID.
 nextCellName :: (Monad m) => PropNetT m TMS.Name
@@ -107,13 +113,25 @@ randomSeed = do
   rng <- initStdGen
   modify $ \s -> s {rng = rng}
 
+getRandomR :: (Random a, Monad m) => (a, a) -> PropNetT m a
+getRandomR r = do
+  s <- get
+  let (i, rng) = randomR r s.rng
+  put (s {rng = rng})
+  pure i
+
 -- | Pick a random element from a list using the monad's internal RNG
 pickRandom :: (Monad m) => [a] -> PropNetT m a
 pickRandom xs = do
-  s <- get
-  let (i, rng) = randomR (0, length xs - 1) s.rng
-  put (s {rng = rng})
+  i <- getRandomR (0, length xs - 1)
   pure (xs !! i)
+
+shuffle :: (Monad m) => [a] -> PropNetT m [a]
+shuffle [] = pure []
+shuffle xs = fmap (Shuffle.shuffle xs) (randseq (length xs - 1))
+  where
+    randseq 0 = pure []
+    randseq i = liftM2 (:) (getRandomR (0, i)) (randseq (i - 1))
 
 branch :: (PrimMonad m, Bounded a, Enum a) => LogicCell (PropNetT m) (OneOf a) -> PropNetT m ()
 branch c = do
@@ -175,3 +193,52 @@ searchDebug cells callback = do
 type PropNetIO a = PropNetT IO a
 
 type PropNetST s a = PropNetT (ST s) a
+
+solution :: (MonadPropNet m) => [Cell m (OneOf a)] -> m (Either (Cell m (OneOf a)) [a])
+solution cells = iter cells (Right [])
+  where
+    iter [] res = pure res
+    iter (c : cs) (Right vs) = do
+      val <- peek c
+      case only val of
+        Nothing -> iter cs (Left c)
+        Just v -> iter cs (Right (v : vs))
+    iter (c : cs) (Left b) = do
+      best <- peek b
+      val <- peek c
+      if OneOf.size val > 1 && OneOf.size val < OneOf.size best
+        then iter cs (Left c)
+        else iter cs (Left b)
+
+searchDFS ::
+  (PrimMonad m, Eq a, Bounded a, Enum a, Show a) =>
+  Premise ->
+  [Cell (PropNetT m) (OneOf a)] ->
+  (PropNetT m) (Maybe [a])
+searchDFS prem cells = do
+  res <- solution cells
+  case res of
+    Right vs -> pure (Just (reverse vs))
+    Left branchPt -> do
+      backup <- traverse peek cells
+      ps <- peek branchPt >>= shuffle . OneOf.toList
+
+      let tryBranch value = do
+            let prem' = addAssumption (Assumption (cellName branchPt) (fromEnum value)) True prem
+            push branchPt (OneOf.singleton value)
+            s <- get
+            if s.contradiction
+              then do
+                zipWithM_ replace cells backup
+                pure Nothing
+              else searchDFS prem' cells
+
+      jank tryBranch ps
+
+jank :: (Monad m) => (a -> m (Maybe b)) -> [a] -> m (Maybe b)
+jank _ [] = pure Nothing
+jank f (x : xs) = do
+  r <- f x
+  case r of
+    Nothing -> jank f xs
+    _ -> pure r
