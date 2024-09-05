030355c368dc683c50bf7332980678082e84f7e7
holzmanj <jesseholzman96@gmail.com>
2024-09-05 10:09:15 -0400

    Tidy up and add some comments

--- examples/Tiles.hs
+++ examples/Tiles.hs
@@ -9,7 +9,7 @@ import Data.List (transpose)
 import Data.Maybe (fromMaybe)
 import Data.PropNet.Partial.Combination (Combination)
 import qualified Data.PropNet.Partial.Combination as C
-import Data.PropNet.Partial.OneOf (OneOf, only)
+import Data.PropNet.Partial.OneOf (OneOf)
 import qualified Data.PropNet.Partial.OneOf as OneOf
 import Data.PropNet.Relation (BinaryR)
 
@@ -39,15 +39,18 @@ showTile x = case C.toList x of
   [N, S, W, E] -> "╬"
   _ -> "?" -- tiles with one connection should be rejected
 
+-- Exclusively matches specific elements of two combinations:
+--
+-- `x` contains `ex` if and only if `y` contains `ey`
 connect :: a -> a -> BinaryR (OneOf (Combination a)) (OneOf (Combination a))
-connect i j (x, y) = (x', y')
+connect ex ey (x, y) = (x', y')
   where
-    x' = connect' i j (x, y)
-    y' = connect' j i (y, x)
+    x' = connect' ex ey (x, y)
+    y' = connect' ey ex (y, x)
 
     connect' e1 e2 (c1, c2) = fromMaybe c1 $ do
-      c <- only c2
-      Just $
+      c <- OneOf.only c2
+      pure $
         if C.member e2 c
           then OneOf.filter (C.member e1) c1
           else OneOf.filter (C.notMember e1) c1
@@ -61,18 +64,19 @@ generateTiles = do
   randomSeed
   cells <- replicateM (height * width) empty
 
-  -- tiles cannot have only one connection
+  -- no tiles with only one connection
   let validTiles = OneOf.filter (\x -> C.size x /= 1) OneOf.universal
   traverse_ (`push` validTiles) cells
 
+  -- enforce constraints for keeping tiles "connected"
   let rows = chunksOf width cells
   let cols = transpose rows
-
   for_ (concat $ zipWith zip rows (drop 1 rows)) (enforceBinary (connect S N))
   for_ (concat $ zipWith zip cols (drop 1 cols)) (enforceBinary (connect E W))
 
+  -- draw each partial solution as we search
   searchDebug cells $ \vals -> do
-    let tiles = maybe "_" showTile . only <$> vals
+    let tiles = maybe "_" showTile . OneOf.only <$> vals
     let text = unlines $ concat <$> chunksOf width tiles
     liftIO (putStrLn $ text ++ "\ESC[21F")
 
